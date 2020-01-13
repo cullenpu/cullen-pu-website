5f8238bd491f83474c541e6585dae44769bf71df
Cullen Pu <cullen.pu@mail.utoronto.ca>
2020-01-12 19:35:50 -0500

    Updates

---
+++ precache-manifest.5a34c12a6d544fac1ae5b36ec1513798.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5438ec9a251c51664429b33ae72105de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb1611d96ef95b08ba13",
+    "url": "/static/css/main.b4e56416.chunk.css"
+  },
+  {
+    "revision": "07c0c76244400291de53",
+    "url": "/static/js/2.a5ac3209.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a5ac3209.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb1611d96ef95b08ba13",
+    "url": "/static/js/main.f54c8f5f.chunk.js"
+  },
+  {
+    "revision": "dda1d03d02a1431c3829",
+    "url": "/static/js/runtime-main.18fe50c1.js"
+  },
+  {
+    "revision": "d64381f06e359ec8d66dcd705cee92b6",
+    "url": "/static/media/Resume (Toronto).d64381f0.pdf"
+  },
+  {
+    "revision": "03b5a587ff7152413b483d4734871bba",
+    "url": "/static/media/boston.03b5a587.jpg"
+  },
+  {
+    "revision": "04f3b3a3c02ac07e5418e511a0795604",
+    "url": "/static/media/image1.04f3b3a3.jpg"
+  },
+  {
+    "revision": "00c0a0b58665e34bef8b9f530f8676a7",
+    "url": "/static/media/image2.00c0a0b5.jpg"
+  },
+  {
+    "revision": "1f729e954c8687e98666f18664c85fe3",
+    "url": "/static/media/image3.1f729e95.jpg"
+  },
+  {
+    "revision": "936c8b33826c7eaf0d8ddd4c2e496d06",
+    "url": "/static/media/image4.936c8b33.jpg"
+  },
+  {
+    "revision": "8bd69941da21f0bc7cbee522d3e86eea",
+    "url": "/static/media/meal-prep-4-U.8bd69941.png"
+  },
+  {
+    "revision": "594c1cdb4a8e44e7a313c2224e038389",
+    "url": "/static/media/politic-game.594c1cdb.jpg"
+  }
+]);
